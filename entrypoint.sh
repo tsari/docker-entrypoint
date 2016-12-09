486a69c4155245d90b91dc6f46083da456c057f6
Tibor Sari <tibor.sari@verivox.com>
2016-12-09 16:28:10 +0100

    execute as root if user creation failed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # general good practice (stop on error, missing variables):
 set -eu
-groupadd --system --gid=$GID $USER && useradd --system --gid=$GID --uid=$UID $USER && \
-exec sudo --preserve-env -u $USER "$@"
+groupadd --system --gid=$GID $USER && useradd --system --gid=$GID --uid=$UID $USER >> /dev/null
+
+if [ $? = 0 ]; then
+    exec sudo --preserve-env -u $USER "$@"
+else
+    exec "$@"
+fi
