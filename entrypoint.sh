715fc256ed6239d4a1ac262b5f8888c6b0201710
Tibor Sari <tibor.sari@verivox.com>
2016-12-13 09:53:15 +0100

    don't stop when vars are missing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# general good practice (stop on error, missing variables):
-set -eu
+# general good practice (stop on error):
+set -e
 if [ ! -z $GID ]; then
     groupadd --system --gid=$GID $USER && useradd --system --gid=$GID --uid=$UID $USER
     exec sudo --preserve-env -u $USER "$@"
