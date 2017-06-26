717a899a42e6041c289da98f0275510cedb632e9
Hans-Peter Buniat <hans-peter.buniat@unister.de>
2017-06-26 14:33:10 +0200

    if exists by id, rename and set $HOME

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,23 @@
 # general good practice (stop on error):
 set -e
 
-function createUser(){
+function createUser() {
     useradd --system --gid=$GID --uid=$UID $USER
 }
 
-function createGroup(){
+function createGroup() {
     groupadd --system --gid=$GID $USER
 }
 
-function getUserById(){
+function renameUser() {
+    usermod -l $USER -d /home/$USER $1
+}
+
+function getUserById() {
     echo $(getent passwd "$UID" | awk -F: '{print $1}')
 }
 
-function getGroupById(){
+function getGroupById() {
     echo $(getent group "$GID" | awk -F: '{print $1}')
 }
 
@@ -36,6 +40,10 @@ else
         existingUser=$(getUserById)
     fi
 
+    if [ $existingUser != $USER ]; then
+        renameUser $existingUser
+    fi
+
     # execute as non-root user
-    exec sudo -E -u "$existingUser" "$@"
+    exec sudo -E -u "$USER" "$@"
 fi
